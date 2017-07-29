ba4ffd4077f4192d9f8795197a95e41b9ffa2be2
Sylwester Herber <xslyherx@gmail.com>
2017-07-29 22:55:48 +0200

    Changed tactory invoker to working condition, window size adjustments, added HPC and first bid addend function counting  cart points and size of longest suit removed unnecesary variables fixed ConverToString usage in main program

--- WpfCAcademy/Factory/BridgeFirstBid.cs
+++ WpfCAcademy/Factory/BridgeFirstBid.cs
@@ -10,12 +10,20 @@ namespace WpfCAcademy.Factory
 {
     public static class BridgeFirstBid
     {
-        public enum Conventions : int {Natural, StrongPass};
+        public enum Conventions : int { Natural, StrongPass };
+        /*
+         * https://stackoverflow.com/questions/801070/dynamically-invoking-any-function-by-passing-function-name-as-string
+         */
         public static string PredictFirstBid(BridgeHand Hand, Conventions Convention)
         {
-            string FactoryClassName = Enum.GetName(typeof(Conventions), Convention)+"FirstBid";
-            MethodInfo method = Type.GetType(FactoryClassName).GetType().GetMethod("PredictFirstBid");
-            object result = method.Invoke(Hand, null);
+            object[] methodArgs = new object[1];
+            methodArgs[0] = Hand;
+            object[] constructorArgs = new object[0];
+            string FactoryClassName = "WpfCAcademy.Factory." + Enum.GetName(typeof(Conventions), Convention) + "FirstBid";
+            var type = Type.GetType(FactoryClassName);
+            object instance = Activator.CreateInstance(type, constructorArgs);
+            MethodInfo method = type.GetMethod("PredictFirstBid");
+            object result = method.Invoke(instance, methodArgs);
             return result.ToString();
         }
     }
